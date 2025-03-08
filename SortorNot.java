55f36464597aac537d70e31d9e71a2f43e47a8ed
shejaldeogade11 <shejaldeogade11@gmail.com>
2025-03-08 21:36:50 +0530

    Update SortorNot.java

--- SortorNot.java
+++ SortorNot.java
@@ -5,33 +5,13 @@ public class SortorNot {
         
     }
     public static void  sort(int a[] ) {
-        // for(int i=0;i<a.length;i++){
-        //   for(int j=1;j<a.length;j++){
-        //     if(a[i] >a[j])
-        //     return false;
-        // }  
-        // }
-        // return true;
- 
-        // for(int i=0;i<a.length;i++){
-        //     if(a[i] >a[i+1])
-        //     return false;
-        // }
-        //return true;
-        int arr[] = new int[6];
-        for(int i=0;i<a.length;i++){
-           for(int j=0;j<a.length-1;j++){
-            if(a[j]==0 && a[j+1]!=0){
-                int temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-           
-           }
-        }
-        for(int i=0;i<a.length;i++){
-System.out.println(a[i]);
-    }
+         for(int i=0;i<a.length;i++){
+           for(int j=1;j<a.length;j++){
+            if(a[i] >a[j])
+            return false;
+         }  
+         }
+         return true;
 
     }
 }
